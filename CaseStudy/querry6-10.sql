81517a63bfc454f8491f26199ca3215ed1af7a55
anhhuy12345 <thienhung190492@gmail.com>
2022-12-21 09:26:10 +0700

    cs

--- CaseStudy/querry6-10.sql
+++ CaseStudy/querry6-10.sql
@@ -62,8 +62,8 @@ select distinct khach_hang.ho_ten from khach_hang
 group by khach_hang.ho_ten ;
 
 -- 9.	Thực hiện thống kê doanh thu theo tháng, 
--- nghĩa là tương ứng với mỗi tháng trong năm 2021 thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng.
-select month(hop_dong.ngay_lam_hop_dong) as `# thang`, count(month(hop_dong.ngay_lam_hop_dong)) as `so_luong_khach_hang`
+-- nghĩa là tương ứng với mỗi tháng trong năm 2021 thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng
+select month(hop_dong.ngay_lam_hop_dong) as `# thang`, count(ma_khach_hang) as `so_luong_khach_hang`
 from hop_dong
 where year(hop_dong.ngay_lam_hop_dong) = "2021"
 group by month(hop_dong.ngay_lam_hop_dong)
